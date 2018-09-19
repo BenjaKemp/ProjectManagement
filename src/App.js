7e1d7a250d6ec4f0a8d6d3b97664ee41edcabd72
BenjaKemp <BenjoKemp@gmail.com>
2018-09-19 13:11:51 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+    <h2>this is the start of the app</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
